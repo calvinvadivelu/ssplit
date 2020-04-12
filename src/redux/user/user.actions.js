213a7bc9c642efa6a7d36c7ede8c4a36156b9dd5
cvadivelu <50369619+cvadivelu@users.noreply.github.com>
2020-04-12 17:20:17 -0400

    allowing guest login in sharers login screen

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -2,3 +2,8 @@ export const setCurrentUser = user => ({
     type: 'SET_CURRENT_USER',
     payload: user,
 })
+
+export const setGuestUser = user => ({
+    type: 'SET_GUEST_USER',
+    payload: user,
+})
