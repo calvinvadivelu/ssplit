608a632a7f8044e739640d2c19ecd3cb5904c4c6
Calvin Vadivelu <cvadivelu@gmail.com>
2020-04-26 21:17:48 -0400

    adding temp pic data to SUBSCRIP DATA

--- src/constants/SUB_DATA.js
+++ src/constants/SUB_DATA.js
@@ -7,7 +7,7 @@
 */
 
 const SUBSCRIPTION_DATA = [
-    { id: 1, name: 'Netflix', description: 'Movie Streaming Subscription', picture: '', plans: [
+    { id: 1, name: 'Netflix', description: 'Movie Streaming Subscription', picture: 'https://cdn.vox-cdn.com/thumbor/lfpXTYMyJpDlMevYNh0PfJu3M6Q=/39x0:3111x2048/920x613/filters:focal(39x0:3111x2048):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/49901753/netflixlogo.0.0.png', plans: [
         {planName: 'Basic', pricePerMonth: 8},
         {planName: 'Standard', pricePerMonth: 12},
         {planName: 'Premium', pricePerMonth: 16}
@@ -25,7 +25,5 @@ const SUBSCRIPTION_DATA = [
     { id: 7, name: 'Temp Subscription', description: 'Subscription For Something', pricePerMonth: 15, picture: ''},
     { id: 8, name: 'Temp Subscription', description: 'Subscription For Something', pricePerMonth: 15, picture: ''},
     { id: 9, name: 'Temp Subscription', description: 'Subscription For Something', pricePerMonth: 15, picture: ''},
-
-
 ]
 export default SUBSCRIPTION_DATA
