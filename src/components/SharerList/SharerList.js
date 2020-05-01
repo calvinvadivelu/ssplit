8eabc4a92e6bb867fb8adc31ab7e1e4f44a0e919
Calvin Vadivelu <cvadivelu@gmail.com>
2020-05-01 14:36:34 -0400

    updating SharerList to show errors on fields

--- src/components/SharerList/SharerList.js
+++ src/components/SharerList/SharerList.js
@@ -2,31 +2,57 @@ import React from 'react';
 
 import './SharerList.scss';
 
+import verifyEmailField from '../../helper/verifyEmailField';
 import userIcons from '../../images/userIcons';
 
-const SharerList = ({ sharers, editSharer, removeSharer }) => {
+const SharerList = ({ sharers, editSharer, removeSharer, addSharer }) => {
     const remove = (e, index) => {
         e.preventDefault();
         removeSharer(index)
     }
+
+    const handleAddSharer = () => {
+        let sharersAreGood = true
+        editSharer(sharers.map(sharer => {
+            if (!verifyEmailField(sharer.email)) {
+                sharer.emailError = true
+                sharersAreGood = false
+            }
+            if (sharer.name.length === 0){
+                sharer.nameError = true
+                sharersAreGood = false
+            }
+            return sharer
+        }))
+        if (sharersAreGood) addSharer()
+    }
+
+    const handleEditSharer = (sharer, fieldType, index) => {
+        // if (sharers[index][fieldType]) sharers[index][fieldType] = false
+        editSharer(sharer, index)
+    }
     return (
-        <div className="sharerslist" style={{height : sharers.length * 82 + 16 + 'px'}}>
-            {sharers.map((sharer, key) => 
-                <div className="sharerslist-sharer" key={key}>
-                    <div className="sharerslist-sharer__img" style={{backgroundImage: `url(${userIcons[key]})`}}></div>
-                    <div className="sharerslist-sharer__details">
-                        <div className="sharerslist-sharer__details-name">
-                            <input type="text" autoComplete="new-password" id='sharer-name' placeholder='Name' value={sharer.name} onChange={(e) => editSharer({name: e.target.value, email: sharer.email}, key)}/>
+        <div className="sharerslist">
+            <label htmlFor="sharerslist-label">Who are you planning on sharing this subscription with?</label>
+            <div className="sharerslist-content" style={{height : sharers.length * 82 + 16 + 'px'}}>
+                {sharers.map((sharer, key) => 
+                    <div className="sharerslist-content-sharer" key={key}>
+                        <div className="sharerslist-content-sharer__img" style={{backgroundImage: `url(${userIcons[key]})`}}></div>
+                        <div className="sharerslist-content-sharer__details">
+                            <div className="sharerslist-content-sharer__details-name">
+                                <input type="text"  className={`${sharer.nameError && 'invalid'}`} autoComplete="new-password" id='sharer-name' placeholder='Name' value={sharer.name} onChange={(e) => handleEditSharer({name: e.target.value, email: sharer.email}, 'nameError', key)}/>
+                            </div>
+                            <div className="sharerslist-content-sharer__details-email">
+                                <input type="email" className={`${sharer.emailError && 'invalid'}`} autoComplete="new-password" id={'sharer-email'} placeholder='Email' value={sharer.email} onChange={(e) => handleEditSharer({name: sharer.name, email: e.target.value}, 'emailError', key)}/>
+                            </div>
                         </div>
-                        <div className="sharerslist-sharer__details-email">
-                            <input type="email" autoComplete="new-password" id='sharer-email' placeholder='Email' value={sharer.email} onChange={(e) => editSharer({name: sharer.name, email: e.target.value}, key)}/>
+                        <div className="sharerslist-content-sharer__delete">
+                            {sharers.length > 1 && <button onClick={(e) => remove(e, key)}>X</button>}
                         </div>
                     </div>
-                    <div className="sharerslist-sharer__delete">
-                        {sharers.length > 1 && <button onClick={(e) => remove(e, key)}>X</button>}
-                    </div>
-                </div>
-            )}
+                )}
+            </div>
+            {sharers.length < 5 && <button type='button' onClick={handleAddSharer}>Add Another Sharer?</button>}
         </div>
     );
 };
