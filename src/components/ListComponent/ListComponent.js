152e0cd5b59493e8abef54f093841122992a44ca
calvinvadivelu <cvadivelu@gmail.com>
2020-05-06 22:30:57 -0400

    handling enter on search bar

--- src/components/ListComponent/ListComponent.js
+++ src/components/ListComponent/ListComponent.js
@@ -2,26 +2,23 @@ import React, { useState } from 'react';
 
 import ListItem from './ListItem/ListItem';
 import './ListComponent.scss';
-/*
-props.contents: {
-    name: String,
-    description: String,
-    picture: url,
-    pricePerMonth: Number
-}
-*/
 
 const ListComponent = ({ contents, activePlan, setActivePlan }) => {
     const [search, updateSearch] = useState('')
     const [isPlansExpanded, setExpandedHeight] = useState(false)
     const listItems = contents.filter(item => item.name.toLowerCase().startsWith(search.toLowerCase()))
+
+    const handleEnter = (e) => {
+        if (e.key !== 'Enter') return;
+        document.querySelector('.listitem__btncontainer').focus();
+    }
     return (
         <div className="listbox">
             <label className="listbox-label">
             What is the subscription service you are sharing?
             </label>
             <div className="listbox-searchbox">
-                <input type="search" value={search} onChange={(e) => updateSearch(e.target.value)} />
+                <input type="search" value={search} onChange={(e) => updateSearch(e.target.value)}  onKeyPress={handleEnter}/>
             </div>
             <div className='listbox-contents' style={{height: `${((listItems.length) * 82) + isPlansExpanded * 68}px`}}>
                 {listItems.map((item, idx) => {
