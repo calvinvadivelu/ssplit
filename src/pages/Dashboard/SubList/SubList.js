495e75ca0326d04d14470e1841e8af7843ff312a
calvinvadivelu <cvadivelu@gmail.com>
2020-05-22 13:49:06 -0400

    removing sharer data from sublist component

--- src/pages/Dashboard/SubList/SubList.js
+++ src/pages/Dashboard/SubList/SubList.js
@@ -11,7 +11,6 @@ const SubList = ({ subscriptions, setActivePlan, activePlan }) => {
         <div className='sublist'>
             {subscriptions.map((subscriptionPlanData, index) => {
                 const subscriptionData = SUB_DATA[subscriptionPlanData.dataIndex]
-                console.log('subscriptionPlanData.sharers :>> ', subscriptionPlanData.sharers);
                 return (
                     <button key={index} id={`sub-${index}`} className="sublist__sub-btncontainer" onClick={() => setActivePlan(index)}>
                         <div className={`sublist__sub ${isActivePlan && 'minimized'}`} style={ index === activePlan ? {backgroundColor: `${subscriptionData.colourCode}`} : {}}>
@@ -21,9 +20,6 @@ const SubList = ({ subscriptions, setActivePlan, activePlan }) => {
                             {!isActivePlan && <>
                                 <label htmlFor={`sub-${index}`} className='sublist__sub-name'>{subscriptionPlanData.name}</label>
                                 <div className='sublist__sub-total' ><b>${subscriptionPlanData.totalPrice}</b>/month</div>
-                                <div className="sublist__sub-sharers">
-                                    {subscriptionPlanData.sharers.reduce((paidSharers, sharer) => sharer.paid ? ++paidSharers : paidSharers, 0)}/{subscriptionPlanData.sharers.length} Paid
-                                </div>
                             </>}
                         </div>
                     </button>
